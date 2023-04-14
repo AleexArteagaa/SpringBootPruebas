9fe0c4c53f70b3089f1e0d3ee8cd0dac55ad6934
aarteagaa <aarteagaa@11LAPDKXS1T3.indra.es>
2023-04-14 11:44:52 +0200

    user-service

---
+++ user-service/src/main/java/com/example/userservice/entity/User.java
@@ -0,0 +1,28 @@
+package com.example.userservice.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@Table(schema = "users_service", name = "users")
+public class User {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private int id;
+	@Column(name = "name", length = 100, nullable = false)
+	private String name;
+	@Column(name = "email", length = 100, nullable = true)
+	private String email;
+
+}
