4ec8fba6633eb5476c5b497d3f632c2e21906fe5
aarteagaa <aarteagaa@11LAPDKXS1T3.indra.es>
2023-04-18 13:49:29 +0200

    feign client fix

--- user-service/src/main/java/com/example/userservice/feignclients/CarFeignClient.java
+++ user-service/src/main/java/com/example/userservice/feignclients/CarFeignClient.java
@@ -10,12 +10,12 @@ import org.springframework.web.bind.annotation.RequestBody;
 
 import com.example.userservice.model.Car;
 
-@FeignClient(name = "car-service", url = "http://localhost:8002/car")
+@FeignClient(name = "car-service")
 public interface CarFeignClient {
 
-	@PostMapping()
+	@PostMapping("/car")
 	Car save(@RequestBody Car car);
 
-	@GetMapping("/byuser/{userId}")
+	@GetMapping("/car/byuser/{userId}")
 	List<Car> getCars(@PathVariable("userId") int userId);
 }
